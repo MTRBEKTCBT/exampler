77abee94b23b23adfc0eeea5502bc33c6f6fe310
Ryoto Ozeki <ryotoozeki@gmail.com>
2022-05-22 18:10:05 +0900

    Add: tests

--- tests/testthat/test-count_business_days.R
+++ tests/testthat/test-count_business_days.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("2022年現在の祝日の日数", {
+  expect_equal(zipangu::jholiday(2022) |> length(), 16)
+})
+
+test_that("2024年はうるう年になる" , {
+  expect_equal(seq_days_in_months(2024, 1, 12) |> length(), 366)
 })
